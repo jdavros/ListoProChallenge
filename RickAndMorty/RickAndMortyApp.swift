790eea0ed61160116cca6939d5955559c487e13f
Jose Davalos Rosas <jose.davalos@sonos.com>
2023-07-29 20:36:36 -0700

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by José Dávalos Rosas on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
